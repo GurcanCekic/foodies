9a19496cfe7929785e4057fc9a6cc67b2836b28d
Gurcan Cekic <gcekic@gmail.com>
2026-05-10 15:58:10 +0300

    sqlite implementation finished

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,20 +1,27 @@
 import Link from 'next/link';
 import classes from './page.module.css';
+import MealsGrid from '@/components/meals/meals-grid';
 
-export default function MealsPage() {
+import { getMeals } from '@/lib/meals';
+
+export default async function MealsPage() {
+  const meals = await getMeals();
   return <>
     <header className={classes.header}>
       <h1>
         Delicious meals, created{' '}
         <span className={classes.highlight}>by you</span>
       </h1>
-    </header>
       <p>Choose your recipe and cook it yourself. It is easy and fun.</p>
       <p className={classes.cta}>
         <Link href="/meals/share">
           Share Your Favorite Recipe
         </Link>
       </p>
-    <main className={classes.main}></main>
+    </header>
+
+    <main className={classes.main}>
+      <MealsGrid meals={meals} />
+    </main>
   </>
 }
