18c9949c94fa2d813933092f739d008c7ee033d6
Gurcan Cekic <gcekic@gmail.com>
2026-05-16 20:05:09 +0300

    image picker added

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,5 +1,41 @@
+import classes from './page.module.css';
+import Image from 'next/image';
+
+import { getMeal } from '@/lib/meals';
+import { notFound } from 'next/navigation';
+
 export default async function MealDetailsPage({params}) {
-    const { mealSlug } = await params;
-    console.log("mealSlug: ", mealSlug);
-  return <h1>Meal Details Page</h1>
+
+  const { mealSlug } = await params;
+  const meal = getMeal(mealSlug);
+
+  if (!meal) {
+    notFound();
+  }
+  
+  meal.instructions = meal.instructions.replace(/\n/g, '<br />')
+
+  return <>
+    <header className={classes.header}>
+      <div className={classes.image}>
+        <Image src={meal.image} alt={meal.title} fill loading="eager"/>
+      </div>
+      <div className={classes.headerText}>
+        <h1>{meal.title}</h1>
+        <p className={classes.creator}>
+          by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+        </p>
+        <p className={classes.summary}>{meal.summary}</p>
+      </div>
+    </header>
+    <main>
+      <p className={classes.instructions} dangerouslySetInnerHTML={
+        {
+          __html: meal.instructions
+        }
+      }>
+
+      </p>
+    </main>
+  </>
 }
